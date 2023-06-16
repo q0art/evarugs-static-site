e285ad6b876415d5875adeef5265a53fa3499ffb
qtqt.art <qtqt.art@gmail.com>
2023-06-16 10:02:05 +0300

    add header

--- configuration/environment.js
+++ configuration/environment.js
@@ -1,22 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  paths: {
-    /* Path to source files directory */
-    source: path.resolve(__dirname, '../src/'),
-
-    /* Path to built files directory */
-    output: path.resolve(__dirname, '../dist/'),
-  },
-  server: {
-    host: 'localhost',
-    port: 8000,
-  },
-  limits: {
-    /* Image files size in bytes. Below this value the image file will be served as DataURL (inline base64). */
-    images: 8192,
-
-    /* Font files size in bytes. Below this value the font file will be served as DataURL (inline base64). */
-    fonts: 8192,
-  },
+	paths: {
+		source: path.resolve(__dirname, '../src/'),
+		output: path.resolve(__dirname, '../dist/'),
+	},
+	server: {
+		host: 'localhost',
+		port: 8000,
+	},
+	limits: {
+		images: 8192,
+		fonts: 8192,
+	},
 };
--- configuration/webpack.prod.config.js
+++ configuration/webpack.prod.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -6,28 +5,20 @@ const TerserPlugin = require('terser-webpack-plugin');
 const webpackConfiguration = require('../webpack.config');
 
 module.exports = merge(webpackConfiguration, {
-  mode: 'production',
-
-  /* Manage source maps generation process. Refer to https://webpack.js.org/configuration/devtool/#production */
-  devtool: false,
-
-  /* Optimization configuration */
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-      }),
-      new CssMinimizerPlugin(),
-    ],
-  },
-
-  /* Performance treshold configuration values */
-  performance: {
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-
-  /* Additional plugins configuration */
-  plugins: [],
+	mode: 'production',
+	devtool: false,
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				parallel: true,
+			}),
+			new CssMinimizerPlugin(),
+		],
+	},
+	performance: {
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000,
+	},
+	plugins: [],
 });
